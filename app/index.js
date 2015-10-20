0c66bd900a5f8733bcbf12365b370764cbc14405
niko <niko.heikkinen3@gmail.com>
2015-10-20 22:42:14 +0300

    Se toimii jotenkin ! Nodejs+react palvelin

--- app/index.js
+++ app/index.js
@@ -6,30 +6,16 @@ var browserify = require('browserify');
 var ReactDOMServer = require('react-dom/server'),
     DOM = React.DOM, body = DOM.body, div = DOM.div, script = DOM.script;
 
-var Kehys = require('./views/main.js');
+var Kehys = require('./views/index.jsx');
 app.get('/',function(req,res){
  
 res.setHeader('Content-Type', 'text/html');
   res.end(React.renderToStaticMarkup(
     React.DOM.body(
       null,
-      React.DOM.div({
-        id: 'app',
-        dangerouslySetInnerHTML: {
-          __html: React.renderToString(React.createElement(TodoBox, {
-            data: data
-          }))
-        }
-      }),
-      React.DOM.script({
-        'id': 'initial-data',
-        'type': 'text/plain',
-        'data-json': JSON.stringify(data)
-      }),
-      React.DOM.script({
-        src: '/bundle.js'
-      })
-    )
+      React.DOM.div({ id: 'app',dangerouslySetInnerHTML: {__html: React.renderToString(React.createElement(Kehys))}}),
+      React.DOM.script({'id': 'initial-data','type': 'text/plain'}),
+      React.DOM.script({src: '/bundle.js'}))
   ));
 
 });
