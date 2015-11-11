01b05d7bf5164ab202bcb893cd846e57934cb193
Heikkinen Niko <nikheihe@sovproj04.it.jyu.fi>
2015-11-11 15:09:19 +0200

    vitut mitään saanu aikaseksi

--- database/main.js
+++ database/main.js
@@ -1,34 +1,71 @@
 var request = require('request');
 //var async = require('async');
 var myJSON = require('JSON');
-var msql = require('mssgl');
+var sql = require('mysql');
 
-var id = "";
-var key = "";
-
+var id =
+var key =
+var off = 0;
 var testjson ="";
 
 function getMyBody(url, callback) {
-  request({
-    url: url,
-    async:false,
-    json: true
-  }, function (error, response, body) {
-    if (error || response.statusCode !== 200) {
-      return callback(error || {statusCode: response.statusCode});
-    }
-      callback(null, body);  
-  });
+    request({
+	url: url,
+	async:false,
+	json: true
+    }, function (error, response, body) {
+	if (error || response.statusCode !== 200) {
+	    return callback(error || {statusCode: response.statusCode});
+	}
+	callback(null, body);  
+    });
 }
 
-getMyBody('https://external.api.yle.fi/v1/programs/items.json?app_id=' + id + '&app_key='+ key + '&category=5-135&limit=1&availability=ondemand', function(err, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(body.data);
-    testjson = body.data[0].title;
-    console.log(testjson);
-  }
+function kissa(){
+getMyBody('https://external.api.yle.fi/v1/programs/items.json?app_id=' + id + '&app_key='+ key + '&category=5-131&availability=ondemand&mediaobject=video&type=TVProgram', function(err, body) {
+    if (err) {
+	console.log(err);
+    } else {
+        console.log(body.meta.count);
+	listaa( body.meta.count);
+    }
 });
-console.log(testjson);
+}
 
+function listaa(limit){
+    getMyBody('https://external.api.yle.fi/v1/programs/items.json?app_id=' + id + '&app_key='+ key + '&category=5-131&availability=ondemand&mediaobject=video&type=TVProgram&offset=650&limit=' + 100, function(err, body) {
+	if (err) {
+	    console.log(err);
+	} else {
+
+            var con = sql.createConnection({
+		 host     : 'localhost',
+		 user     : 'root',
+		 password : '',
+		 database : 'db'
+
+	    });
+
+	    con.connect((function(err) {
+                if(!err){
+		    console.log("homma kusee");}
+		else{
+		    console.log(err)}
+	    }));
+
+            for(var i = 0; i < 100; i++){
+		var suomiNimi = body.data[i].title.fi;
+		var id = body.data[i].id;
+		var imageId =  body.data[i].image.id;
+		var orgTitle = body.data[i].originalTitle.unknown;
+		//onnection.query('INSERT INTO elokuvat VALUES ('+ id + ',' + orgTitle + ',' + suomiNimi +','+ imageId+');');
+		con.query('INSERT INTO elokuvat VALUES (asdfasdfs,asdfdsa,asdafds,adsfdas);');
+	    }
+	    
+
+
+	}
+    });}
+
+
+kissa();
--- database/test.sql
+++ database/test.sql
@@ -1,10 +1,10 @@
+DROP TABLE elokuvat;
+
 CREATE TABLE elokuvat (
-  id varchar(9) NOT NULL,
+  id varchar(20) NOT NULL,
   orginalnimi varchar(100),
   suominimi varchar(100),
   imgid varchar(12),
   PRIMARY KEY (id)
 );
 
-INSERT INTO elokuvat (id, orginalnimi,suominimi,imgid) VALUES
-('1-2120545', 'Pasila','Pasila', '13-1-2120545');
